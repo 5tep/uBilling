a993b73a0aedef44e9c3c4ab5c893d4118bf86f5
5tep <98393495+5tep@users.noreply.github.com>
2024-10-23 19:03:05 +0300

    Create REGIONS.sh

---
+++ once-load/REGIONS.sh
@@ -0,0 +1,27 @@
+# Получение текущей даты
+current_date=$(date +%Y%m%d_%H%M)
+
+# Формирование MySQL-запроса с выводом в файл
+echo "
+SELECT 
+    s.id AS ID,
+    '2022-01-01 00:00:00' AS BEGIN_TIME,  --  дата начала
+    '2099-12-31 23:59:59' AS END_TIME,    --  дата окончания
+    s.sityname AS DESCRIPTION,        -- Описание
+    '' AS MCC,                         -- Значение MCC
+    '' AS MNC                           -- MNC не указан
+INTO OUTFILE '/var/lib/mysql-files/REGIONS_$current_date.txt'
+FIELDS TERMINATED BY ';' 
+OPTIONALLY ENCLOSED BY ''
+LINES TERMINATED BY '\n'
+FROM 
+    sity s
+;
+" > /var/lib/mysql-files/query.sql
+
+# Выполнение завроса в базе данных
+mysql -u user -ppassword bdname < /var/lib/mysql-files/query.sql
+
+# Перенос файлов, подчищаем за собой
+mv -f /var/lib/mysql-files/REGIONS* /home/user/COPM/files
+rm -f /var/lib/mysql-files/query.sql
