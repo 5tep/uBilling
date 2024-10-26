a31828c052da9e7663de59409581bccda48b859c
5tep <98393495+5tep@users.noreply.github.com>
2024-10-26 16:57:00 +0300

    Create ABONENT_SERVICE.sh

---
+++ once-load/ABONENT_SERVICE.sh
@@ -0,0 +1,39 @@
+# Получение текущей даты
+current_date=$(date +%Y%m%d_%H%M)
+
+# Формирование MySQL-запроса с выводом в файл
+echo "
+SELECT 
+    u.login AS ABONENT_ID, 
+    c.id AS REGION_ID, 
+    u.Tariff AS ID, 
+    DATE_FORMAT(cd.`date`, '%Y-%m-%d 00:00:00') AS BEGIN_TIME, 
+    '2099-12-31 23:59:59' AS END_TIME, 
+    '' AS PARAMETER, 
+    u.login AS INTERNAL_ID1, 
+    u.login AS INTERNAL_ID2
+INTO OUTFILE '/var/lib/mysql-files/ABONENT_SERVICE_$current_date.txt'
+FIELDS TERMINATED BY ';' 
+OPTIONALLY ENCLOSED BY '\"'
+LINES TERMINATED BY '\n'
+FROM 
+    address adr, apt ap, build b, street s, city c, users u
+LEFT JOIN 
+    phones p ON p.login = u.login
+LEFT JOIN 
+    pononu mac ON mac.login = u.login
+LEFT JOIN
+	nethosts net ON net.ip = u.ip
+LEFT JOIN 
+    contracts cn ON cn.login = u.login
+LEFT JOIN 
+    contractdates cd ON cd.contract = cn.contract
+WHERE u.login = adr.login AND adr.aptid = ap.id AND b.id = ap.buildid AND s.id = b.streetid AND c.id = s.cityid;
+" > /var/lib/mysql-files/query.sql
+
+# Выполнение завроса в базе данных
+mysql -u user -ppassword database < /var/lib/mysql-files/query.sql
+
+# Перенос файлов, подчищаем за собой
+mv -f /var/lib/mysql-files/ABONENT_SERVICE_* /home/COPM/files
+rm -f /var/lib/mysql-files/query.sql
