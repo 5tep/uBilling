6226e5f63975f0c99850e5487e32e4971506a143
5tep <98393495+5tep@users.noreply.github.com>
2024-10-29 20:30:08 +0300

    Update COMMUTATORS.sh

--- once-load/COMMUTATORS.sh
+++ once-load/COMMUTATORS.sh
@@ -30,23 +30,22 @@ SELECT
     '2024-01-01 00:00:00' AS BEGIN_TIME,      -- Указанная дата начала
     '2099-12-31 23:59:59' AS END_TIME,        -- Указанная дата окончания
     s.desc AS DESCRIPTION,                    -- Используем поле name для описания
-    '' AS NETWORK_TYPE,                      
+    4 AS NETWORK_TYPE,                      
     sm.modelname AS SWITCH_TYPE,                       
-    '' AS ADDRESS_TYPE_ID,                   
-    '' AS ADDRESS_TYPE,                      
+    0 AS ADDRESS_TYPE_ID,                   
+    1 AS ADDRESS_TYPE,                      
     '' AS ZIP,                                -- Пустое значение для ZIP
     'Российская Федерация' AS COUNTRY,        -- Статическое значение для страны
     'Херсонсткая область' AS REGION,             -- Статическое значение для региона
     '' AS ZONE,              -- Статическое значение для зоны
     '' AS CITY,                   -- Статическое значение для города
-    s.location AS STREET,                     -- Статическое значение для улицы
+    '' AS STREET,                     -- Статическое значение для улицы
     '' AS BUILDING,                        -- Статическое значение для здания
     '' AS BUILD_SECT,                    -- Статическое значение для секции здания
     '' AS APARTMENT,                        -- Пустое значение для квартиры
-    '' AS UNSTRUCT_INFO,                    -- Пустое значение для дополнительной информации
+    s.location AS UNSTRUCT_INFO,                    -- Пустое значение для дополнительной информации
     '' AS SWITCH_SIGN,                       
-    '' AS REGION_ID                        -- Статическое значение для региона
-
+    99 AS REGION_ID                        -- Статическое значение для региона
 INTO OUTFILE '/var/lib/mysql-files/COMMUTATORS_$current_date.txt'
 FIELDS TERMINATED BY ';' 
 OPTIONALLY ENCLOSED BY ''
