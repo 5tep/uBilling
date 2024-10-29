8711a7e12a4350bec9bafb83e640e94f6fadc100
5tep <98393495+5tep@users.noreply.github.com>
2024-10-29 23:06:59 +0300

    Update ABONENT_SERVICE.sh

--- once-load/ABONENT_SERVICE.sh
+++ once-load/ABONENT_SERVICE.sh
@@ -16,28 +16,27 @@ UNION ALL
 SELECT 
     u.login AS ABONENT_ID, 
     99 AS REGION_ID, 
-    s.`desc` AS ID, 
-    DATE_FORMAT(cd.`date`, '%Y-%m-%d 00:00:00') AS BEGIN_TIME, 
+    s.desc AS ID, 
+    DATE_FORMAT(cd.date, '%Y-%m-%d 00:00:00') AS BEGIN_TIME, 
     '2099-12-31 23:59:59' AS END_TIME, 
     '' AS PARAMETER, 
     '' AS INTERNAL_ID1, 
     '' AS INTERNAL_ID2
-INTO OUTFILE '/var/lib/mysql-files/ABONENT_SERVICE_$current_date.txt'
+INTO OUTFILE '/home/boss/COPM/files/ABONENT_SERVICE_$current_date.txt'
 FIELDS TERMINATED BY ';' 
 OPTIONALLY ENCLOSED BY '\"'
 LINES TERMINATED BY '\n'
 FROM 
     nethosts nh, services s, users u
 LEFT JOIN 
-    contracts cn ON cn.`login` = u.`login`
+    contracts cn ON cn.login = u.login
 LEFT JOIN 
     contractdates cd ON cd.contract = cn.contract
 WHERE u.ip = nh.ip AND s.netid = nh.netid;
-" > /var/lib/mysql-files/query.sql
+" > /home/boss/COPM/query.sql
 
 # Выполнение завроса в базе данных
-mysql -u user -ppassword database < /var/lib/mysql-files/query.sql
+mysql -u root -pghbdtn5235441 stg < /home/boss/COPM/query.sql
 
 # Перенос файлов, подчищаем за собой
-mv -f /var/lib/mysql-files/ABONENT_SERVICE_* /home/COPM/files
-rm -f /var/lib/mysql-files/query.sql
+rm -f /home/boss/COPM/query.sql
