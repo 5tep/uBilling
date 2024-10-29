f520bb0e794eeda3271d2e1d3090c82956cb29c6
5tep <98393495+5tep@users.noreply.github.com>
2024-10-29 20:24:00 +0300

    Update ABONENT_SERVICE.sh
    по документации

--- once-load/ABONENT_SERVICE.sh
+++ once-load/ABONENT_SERVICE.sh
@@ -15,30 +15,24 @@ SELECT
 UNION ALL
 SELECT 
     u.login AS ABONENT_ID, 
-    c.id AS REGION_ID, 
-    u.Tariff AS ID, 
+    99 AS REGION_ID, 
+    s.`desc` AS ID, 
     DATE_FORMAT(cd.`date`, '%Y-%m-%d 00:00:00') AS BEGIN_TIME, 
     '2099-12-31 23:59:59' AS END_TIME, 
     '' AS PARAMETER, 
-    u.login AS INTERNAL_ID1, 
-    u.login AS INTERNAL_ID2
+    '' AS INTERNAL_ID1, 
+    '' AS INTERNAL_ID2
 INTO OUTFILE '/var/lib/mysql-files/ABONENT_SERVICE_$current_date.txt'
 FIELDS TERMINATED BY ';' 
 OPTIONALLY ENCLOSED BY '\"'
 LINES TERMINATED BY '\n'
 FROM 
-    address adr, apt ap, build b, street s, city c, users u
+    nethosts nh, services s, users u
 LEFT JOIN 
-    phones p ON p.login = u.login
-LEFT JOIN 
-    pononu mac ON mac.login = u.login
-LEFT JOIN
-	nethosts net ON net.ip = u.ip
-LEFT JOIN 
-    contracts cn ON cn.login = u.login
+    contracts cn ON cn.`login` = u.`login`
 LEFT JOIN 
     contractdates cd ON cd.contract = cn.contract
-WHERE u.login = adr.login AND adr.aptid = ap.id AND b.id = ap.buildid AND s.id = b.streetid AND c.id = s.cityid;
+WHERE u.ip = nh.ip AND s.netid = nh.netid;
 " > /var/lib/mysql-files/query.sql
 
 # Выполнение завроса в базе данных
