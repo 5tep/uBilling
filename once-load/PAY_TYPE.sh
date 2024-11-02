bdec023e2b58e8f44a0b0b8d7ee131f0ba95e38b
5tep <98393495+5tep@users.noreply.github.com>
2024-11-02 15:44:25 +0300

    Update PAY_TYPE.sh

--- once-load/PAY_TYPE.sh
+++ once-load/PAY_TYPE.sh
@@ -1,6 +1,31 @@
 # Получение текущей даты
 current_date=$(date +%Y%m%d_%H%M)
 
-# Формирование пустые данные в файл
-echo "ID;BEGIN_TIME;END_TIME;DESCRIPTION;REGION_ID" > /home/boss/COPM/files/PAY_TYPE_$current_date.txt
-echo "1;2024-01-02 00:00:00;2049-12-31 23:59:00;Касса;99" >> /home/boss/COPM/files/PAY_TYPE_$current_date.txt
+# Формирование MySQL-запроса с выводом в файл
+echo "
+SELECT 
+    'ID',
+    'BEGIN_TIME',
+    'END_TIME',
+    'DESCRIPTION',
+    'REGION_ID'
+UNION ALL
+SELECT 
+    id AS ID, 
+    '2022-12-31 23:59:00' AS BEGIN_TIME, 
+    '2049-12-31 23:59:00' AS END_TIME,
+    cashtype AS DESCRIPTION,
+    99 AS REGION_ID    
+INTO OUTFILE '/home/boss/COPM/files/PAY_TYPE_$current_date.txt'
+FIELDS TERMINATED BY ';' 
+OPTIONALLY ENCLOSED BY ''
+LINES TERMINATED BY '\n'
+FROM 
+    cashtype;
+" > /home/boss/COPM/query.sql
+
+# Выполнение завроса в базе данных
+mysql -u root -pghbdtn5235441 stg < /home/boss/COPM/query.sql
+
+# Перенос файлов, подчищаем за собой
+rm -f /home/boss/COPM/query.sql
