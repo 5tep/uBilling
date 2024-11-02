b3a24f2bfc5509d4085cd4d5dd7b0810d3d8b0ce
5tep <98393495+5tep@users.noreply.github.com>
2024-11-02 14:21:17 +0300

    Create DOC_TYPE.sh

---
+++ once-load/DOC_TYPE.sh
@@ -0,0 +1,6 @@
+# Получение текущей даты
+current_date=$(date +%Y%m%d_%H%M)
+
+# Формирование пустые данные в файл
+echo "DOC_TYPE_ID;BEGIN_TIME;END_TIME;DESCRIPTION;REGION_ID" > /home/boss/COPM/files/DOC_TYPE_$current_date.txt
+echo "1;2008-01-02 00:00:00;2049-12-31 23:59:00;Паспорт;4" >> /home/boss/COPM/files/DOC_TYPE_$current_date.txt
