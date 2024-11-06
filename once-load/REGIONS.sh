07f5cfe718d40081a32b5faf3ba03646902022ab
5tep <98393495+5tep@users.noreply.github.com>
2024-11-06 18:19:35 +0300

    Update REGIONS.sh

--- once-load/REGIONS.sh
+++ once-load/REGIONS.sh
@@ -9,5 +9,5 @@ echo "ID;BEGIN_TIME;END_TIME;DESCRIPTION;MCC;MNC" > /home/boss/COPM/files/REGION
 #
 #   В таблицах данных нет, можно заполнить вручную по примеру ниже:
 #       
-   echo "99;2022-02-02 00:00:00;2049-12-12 23:59:00;ИП;;" >> /home/boss/COPM/files/REGIONS_$current_date.txt
+   echo "8;2022-02-02 00:00:00;2049-12-12 23:59:00;ИП Исаева;;" >> /home/boss/COPM/files/REGIONS_$current_date.txt
 #
