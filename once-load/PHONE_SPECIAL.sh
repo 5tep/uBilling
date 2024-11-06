4e06436083452fedc48586b6c076096ea14ca313
5tep <98393495+5tep@users.noreply.github.com>
2024-11-06 18:19:05 +0300

    Update PHONE_SPECIAL.sh

--- once-load/PHONE_SPECIAL.sh
+++ once-load/PHONE_SPECIAL.sh
@@ -9,5 +9,5 @@ echo "PHONE_NUMBER;DESCRIPTION;BEGIN_TIME;END_TIME;IP_TYPE;IPV4;IPV6;REGION_ID" 
 #
 #   В таблицах данных нет, можно заполнить вручную по примеру ниже:
 #       
-#   echo "79900868634;Техническая поддержка;2009-10-14 00:00:00;2049-12-31 23:59:59;;;;99" >> /home/boss/COPM/files/PHONE_SPECIAL_$current_date.txt
+   echo "79902052100;Техническая поддержка;2009-10-14 00:00:00;2049-12-31 23:59:59;;;;8" >> /home/boss/COPM/files/PHONE_SPECIAL_$current_date.txt
 #
