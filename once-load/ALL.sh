0d48abf0d0e143a50e80a897b6dc5a4101f2fb15
5tep <98393495+5tep@users.noreply.github.com>
2024-12-03 15:13:46 +0300

    Create ALL.sh

---
+++ once-load/ALL.sh
@@ -0,0 +1,27 @@
+#bash
+
+current_date=$(date +%Y%m%d_%H%M)
+
+bash ABONENT.sh $current_date
+
+bash ABONENT_ADDRESS.sh $current_date
+
+bash ABONENT_IDENT.sh $current_date
+
+bash ABONENT_SERVICE.sh $current_date
+
+bash COMMUTATORS.sh $current_date
+
+bash DOC_TYPE.sh $current_date
+
+bash IP_PLAN.sh $current_date
+
+bash PAYMENT.sh $current_date
+
+bash PAY_TYPE.sh $current_date
+
+bash PHONE_SPECIAL.sh $current_date
+
+bash REGIONS.sh $current_date
+
+bash SUPPLEMENTARY_SERVICE.sh $current_date
